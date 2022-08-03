233220199f658e2139c57ffd5659c654fbe31419
unknown <antonioarjpi@gmail.com>
2022-08-03 10:53:51 -0300

    Test FindAll and Save

---
+++ src/test/java/com/devsimple/cloudparking/controller/ParkingControllerTest.java
@@ -0,0 +1,56 @@
+package com.devsimple.cloudparking.controller;
+
+import com.devsimple.cloudparking.controller.dto.ParkingCreateDTO;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerTest {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest(){
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    void whenfindAllCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/parkings")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("license[0]", Matchers.equalTo("MT-1221"));
+    }
+
+    @Test
+    void whenSaveThenCheckIsCreated() {
+        ParkingCreateDTO createDTO = new ParkingCreateDTO();
+        createDTO.setColor("Branco");
+        createDTO.setLicense("License");
+        createDTO.setModel("Renault");
+        createDTO.setState("PI");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/parkings")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("color", Matchers.equalTo("Branco"))
+                .body("model", Matchers.equalTo("Renault"))
+                .body("state", Matchers.equalTo("PI"))
+                .body("license", Matchers.equalTo("License"));
+    }
+
+}
